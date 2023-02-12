dbc03914822d0eb9bc895678ef772fea464fe36d
mrz1880 <mrz1880@gmail.com>
2023-02-12 15:00:06 +0100

    rename LoginForm & RegisterForm to LoginView & RegisterView

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,13 @@
-import RegisterForm from "@/views/RegisterForm.vue"
-import LoginForm from "@/views/LoginForm.vue"
+import RegisterView from "@/views/RegisterView.vue"
+import HomeView from "@/views/HomeView.vue"
+import LoginView from "@/views/LoginView.vue"
 
 export const routes = [
   {
     path: "/",
     name: "home",
-    component: RegisterForm,
+    component: HomeView,
   },
-  { path: "/register", component: RegisterForm },
-  { path: "/login", component: LoginForm },
+  { path: "/register", component: RegisterView },
+  { path: "/login", component: LoginView },
 ]
