a1a858946104cc53b8f9e50faf91c3f0d2a23d9c
mrz1880 <mrz1880@gmail.com>
2023-02-12 15:24:38 +0100

    condition navbar from the authentication status of the user

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,14 +4,16 @@
       <li>
         <router-link to="/">Home</router-link>
       </li>
-      <li>
+      <li v-if="!isAuthenticated">
         <router-link to="/login">Login</router-link>
       </li>
-      <li>
-        <router-link to="/register">Register</router-link>
+      <li v-if="!isAuthenticated">
+        <router-link to="/register">Register </router-link>
       </li>
     </ul>
   </nav>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { isAuthenticated } from "@/composables/storeUser"
+</script>
