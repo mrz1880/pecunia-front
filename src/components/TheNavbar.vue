d4a8b98be92b3da4c82a92bbce69cac4be48512b
mrz1880 <mrz1880@gmail.com>
2023-02-13 20:19:28 +0100

    improve accessibility on TheNavbar component

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,14 +1,20 @@
 <template>
   <nav aria-label="Main Navigation">
     <ul>
-      <li>
-        <router-link aria-current="page" to="/">Home</router-link>
+      <li v-bind:aria-current="isCurrentRoute(routes.home.path)">
+        <router-link :to="routes.home.path">Home</router-link>
       </li>
-      <li v-if="!isAuthenticated">
-        <router-link to="/login">Login</router-link>
+      <li
+        v-if="!isAuthenticated"
+        v-bind:aria-current="isCurrentRoute(routes.login.path)"
+      >
+        <router-link :to="routes.login.path">Login</router-link>
       </li>
-      <li v-if="!isAuthenticated">
-        <router-link to="/register">Register</router-link>
+      <li
+        v-if="!isAuthenticated"
+        v-bind:aria-current="isCurrentRoute(routes.register.path)"
+      >
+        <router-link :to="routes.register.path">Register</router-link>
       </li>
       <li v-if="isAuthenticated">
         <button type="button" @click="logout">Logout</button>
@@ -19,4 +25,12 @@
 
 <script lang="ts" setup>
 import { isAuthenticated, logout } from "@/composables/storeUser"
+import { useRoute } from "vue-router"
+import { routes } from "@/router/routes"
+
+const currentRoute = useRoute().path
+
+function isCurrentRoute(route: string): "page" | null {
+  return currentRoute === route ? "page" : null
+}
 </script>
