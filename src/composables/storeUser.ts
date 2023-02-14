498102314d882f74e00b33a707d75ee39b4f2733
mrz1880 <mrz1880@gmail.com>
2023-02-14 20:51:44 +0100

    update logout to redirect at the end

--- src/composables/storeUser.ts
+++ src/composables/storeUser.ts
@@ -1,10 +1,26 @@
 import { computed, ref } from "vue"
 import type { Tokens } from "@/Interfaces/User"
+import { defaultTokenHelper } from "@/repositories/InMemoryUserRepository"
+import router, { routes } from "@/router"
 
 export const tokens = ref<Tokens>()
 
-export const isAuthenticated = computed(() => !!tokens.value)
+export const isAuthenticated = computed(
+  () => !!tokens.value || localStorage.getItem("tokens")
+)
 
-export function logout(): void {
+export async function logout(): Promise<void> {
   tokens.value = undefined
+  localStorage.removeItem("tokens")
+  await router.push(routes.login.path)
+}
+
+export async function getUserIdFromToken(): Promise<number> {
+  if (!tokens.value) {
+    await router.push(routes.login.path)
+    throw new Error("No tokens")
+  }
+  const { accessToken } = tokens.value
+  const decodedToken = defaultTokenHelper.decodeToken(accessToken)
+  return decodedToken.id
 }
