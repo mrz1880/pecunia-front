0715fc47c267831b2e77dfd1deba679dd2076a33
mrz1880 <mrz1880@gmail.com>
2023-02-14 20:53:31 +0100

    add entry to navbar to go to bankAccountView

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,23 +1,30 @@
 <template>
   <nav aria-label="Main Navigation">
     <ul>
-      <li v-bind:aria-current="isCurrentRoute(routes.home.path)">
+      <li :aria-current="isCurrentRoute(routes.home.path)">
         <router-link :to="routes.home.path">Home</router-link>
       </li>
       <li
+        v-if="isAuthenticated"
+        :aria-current="isCurrentRoute(routes.bankAccount.path)"
+      >
+        <router-link :to="routes.bankAccount.path">Bank Accounts</router-link>
+      </li>
+
+      <li
         v-if="!isAuthenticated"
-        v-bind:aria-current="isCurrentRoute(routes.login.path)"
+        :aria-current="isCurrentRoute(routes.login.path)"
       >
         <router-link :to="routes.login.path">Login</router-link>
       </li>
       <li
         v-if="!isAuthenticated"
-        v-bind:aria-current="isCurrentRoute(routes.register.path)"
+        :aria-current="isCurrentRoute(routes.register.path)"
       >
         <router-link :to="routes.register.path">Register</router-link>
       </li>
       <li v-if="isAuthenticated">
-        <button type="button" @click="logout">Logout</button>
+        <a href="#" @click="logout">Logout</a>
       </li>
     </ul>
   </nav>
@@ -26,11 +33,13 @@
 <script lang="ts" setup>
 import { isAuthenticated, logout } from "@/composables/storeUser"
 import { useRoute } from "vue-router"
-import { routes } from "@/router/routes"
+import { ref } from "vue"
+import { routes } from "@/router"
 
 const currentRoute = useRoute().path
+const showDropDown = ref(false)
 
-function isCurrentRoute(route: string): "page" | null {
-  return currentRoute === route ? "page" : null
+function isCurrentRoute(route: string): "page" | boolean {
+  return currentRoute === route ? "page" : false
 }
 </script>
