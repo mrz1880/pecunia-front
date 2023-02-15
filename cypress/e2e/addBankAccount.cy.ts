99ccbdf23eb1ce31b5a6693ce03ec69476a792fd
mrz1880 <mrz1880@gmail.com>
2023-02-15 09:41:17 +0100

    extract text in a constant

--- cypress/e2e/addBankAccount.cy.ts
+++ cypress/e2e/addBankAccount.cy.ts
@@ -2,13 +2,14 @@ describe("Add Bank Account Use Case", () => {
   it("add bank account", () => {
     cy.signIn()
     cy.get("[data-test='navbar-bankAccount-link']").click()
+    const nomDuCompteBancaire = "nom du compte bancaire"
     cy.get("[data-test='bankAccount-name-input']")
       .clear()
-      .type("nom du compte bancaire")
+      .type(nomDuCompteBancaire)
     cy.get("[data-test='bankAccount-submit-button']").click()
     cy.get("[data-test='bankAccount-list-ul']")
       .first()
-      .contains("nom du compte bancaire")
+      .contains(nomDuCompteBancaire)
       .should("exist")
   })
 })
